cad66e38bf5f82b66c7000de817a7004c5fd489d
Yuichi Saito <munisystem@gmail.com>
2017-10-07 16:06:01 +0900

    Implement function of to change list items using drag and drop

--- entry.js
+++ entry.js
@@ -1,1 +1,44 @@
-document.write('Hello Webpack!!');
+import Vue from 'vue/dist/vue.esm'
+import _ from 'lodash'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        el: '#app',
+        data: {
+            dragItem: undefined,
+            items: [
+                {
+                    name: "Alice",
+                    index: 0
+                },
+                {
+                    name: "Bob",
+                    index: 1
+                },
+                {
+                    name: "Carol",
+                    index: 2
+                }
+            ]
+        },
+        computed: {
+            orderedItems: function () {
+              return _.orderBy(this.items, 'index')
+            }
+        },
+        methods: {
+            dragstart(item, event) {
+                this.dragItem = item;
+                event.target.style.opacity = .5;
+            },
+            dragend(event) {
+                event.target.style.opacity = 1;
+            },
+            dragenter(item) {
+                const tmpIndex = item.index;
+                item.index = this.dragItem.index;
+                this.dragItem.index = tmpIndex;
+            },
+        }
+    });
+});
